da09f1a659e7cc15497348c3ed8302955905b08a
charlie-jay2 <109756523+charlie-jay2@users.noreply.github.com>
2025-08-20 13:36:06 +0100

    Update getLatestMedicalHistory.js

--- netlify/functions/getLatestMedicalHistory.js
+++ netlify/functions/getLatestMedicalHistory.js
@@ -10,7 +10,8 @@ exports.handler = async function (event) {
   }
 
   try {
-    verifyTokenFromHeaders(event.headers);
+    // Verify JWT from headers
+    const user = verifyTokenFromHeaders(event.headers);
 
     const id = event.queryStringParameters && event.queryStringParameters.id;
     if (!id) {
@@ -33,10 +34,51 @@ exports.handler = async function (event) {
       ? medicalHistory[medicalHistory.length - 1]
       : {};
 
+    // Ensure all expected fields exist, even if null
+    const response = {
+      triage: latestRecord.triage || null,
+      primarySurvey: latestRecord.primarySurvey || null,
+      news2Record: latestRecord.news2Record || null,
+      edLocation: latestRecord.edLocation || null,
+      allergies: latestRecord.allergies || null,
+      triageTime: latestRecord.triageTime || null,
+      nurse: latestRecord.nurse || null,
+      overseeingClinician: latestRecord.overseeingClinician || null,
+      directClinician: latestRecord.directClinician || null,
+      specialistClinician: latestRecord.specialistClinician || null,
+      pharmacist: latestRecord.pharmacist || null,
+      ccot: latestRecord.ccot || null,
+      consultingSpeciality: latestRecord.consultingSpeciality || null,
+      admittingSpeciality: latestRecord.admittingSpeciality || null,
+      clinicianNotes: latestRecord.clinicianNotes || null,
+      nursingNotes: latestRecord.nursingNotes || null,
+      imagingOrders: latestRecord.imagingOrders || null,
+      bloodworkOrders: latestRecord.bloodworkOrders || null,
+      news2Score: latestRecord.news2Score || null,
+      heartRate: latestRecord.heartRate || null,
+      bloodPressure: latestRecord.bloodPressure || null,
+      spO2: latestRecord.spO2 || null,
+      respiratoryRate: latestRecord.respiratoryRate || null,
+      temperature: latestRecord.temperature || null,
+      avpu: latestRecord.avpu || null,
+      gcs: latestRecord.gcs || null,
+      presentingComplaint: latestRecord.presentingComplaint || null,
+      historyPresentingComplaint:
+        latestRecord.historyPresentingComplaint || null,
+      pastMedicalHistory: latestRecord.pastMedicalHistory || null,
+      currentMedications: latestRecord.currentMedications || null,
+      differentialDiagnosis: latestRecord.differentialDiagnosis || null,
+      whoDischarged: latestRecord.whoDischarged || null,
+      timeDischarge: latestRecord.timeDischarge || null,
+      outcomePatient: latestRecord.outcomePatient || null,
+      createdAt: latestRecord.createdAt || null,
+      createdBy: latestRecord.createdBy || null,
+    };
+
     return {
       statusCode: 200,
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(latestRecord),
+      body: JSON.stringify(response),
     };
   } catch (err) {
     console.error(err);
