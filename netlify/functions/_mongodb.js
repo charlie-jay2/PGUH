8cbfde983841a7923f4af7bb0c559743480c329c
charlie-jay2 <109756523+charlie-jay2@users.noreply.github.com>
2025-08-23 20:59:38 +0100

    revert

--- netlify/functions/_mongodb.js
+++ netlify/functions/_mongodb.js
@@ -3,21 +3,15 @@ const { MongoClient } = require("mongodb");
 const cached =
   global._mongo_cached || (global._mongo_cached = { client: null, db: null });
 
-async function connectToDatabase() {
+async function connectToDatabase(uri, dbName) {
   if (cached.db) return cached.db;
-
-  const uri = process.env.MONGODB_URI;
-  const dbName = process.env.DB_NAME;
-
   if (!uri || !dbName) throw new Error("Missing DB config");
-
   const client = new MongoClient(uri);
+
   await client.connect();
   const db = client.db(dbName);
-
   cached.client = client;
   cached.db = db;
-
   return db;
 }
 
